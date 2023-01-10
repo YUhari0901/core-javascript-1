081445c5146859a3db55105314eb22532d23acbd
kindtiger <seonbeom2@gmail.com>
2023-01-10 13:23:43 +0900

    [추가] setAttr 유틸 함수

--- client/chapter/dom/03.attr.js
+++ client/chapter/dom/03.attr.js
@@ -76,17 +76,43 @@ console.log( first.dataset.play ); // get
 // get 함수 만들기 
 
 function getAttr(node,prop){
+  // node = '.first'
+  // prop = 'class'
   
   if(typeof node === 'string'){
     node = getNode(node);
   }
 
-  node.getAttribute(prop);
+  return node.getAttribute(prop);
   
 }
 
 
-getAttr('.first','class') // first 
+getAttr(first,'data-play')  
+
+
+
+
+function setAttr(node,prop,value){
+  // validation : 확인 
+  if(typeof node === 'string') node = getNode(node);
+  if(typeof prop !== 'string') throw new TypeError('setAttr 함수의 두 번째 인자는 문자 타입 이어야 합니다.')
+  if(!value) throw new SyntaxError('setAttr 함수의 세 번째 인자는 필수값입니다.')
+  
+  node.setAttribute(prop,value);
+
+}
+
+
+setAttr('.first','data-value','hello');
+
+
+
+
+
+
+
+
 
 
 
