52fe9177165b93e6715b40b5546896b9891872d5
kindtiger <seonbeom2@gmail.com>
2023-01-13 10:21:28 +0900

    [추가] getInputValue 함수 만들기

--- client/main.js
+++ client/main.js
@@ -1,1 +1,63 @@
-console.log('aa');
+
+
+
+/* eslint no-undef:'warn' */
+/* eslint no-unused-vars: 'off' */
+
+
+
+// 1. 인풋 벨류값 가져오기
+// 2. 이벤트 핸들러 연결하기
+// 3. 이벤트 기본동작 차단하기
+// 4. 두 수의 합을 더해주기 
+// 5. 화면에 출력하기
+
+
+
+const firstInput = getNode('#firstNumber');
+const secondInput = getNode('#secondNumber');
+const done = getNode('#done');
+
+
+function getInputValue(node){
+  if(typeof node === 'string') node = getNode(node);
+  if(node.tagName !== 'INPUT') refError('getInputValue 함수는 INPUT ELEMENT만 허용합니다.')
+  return node.value
+}
+
+
+
+function handler(e){
+  e.preventDefault();
+
+  let firstValue = +getInputValue(firstInput);
+  let secondValue = +getInputValue(secondInput);
+  
+  console.log(firstValue, secondValue);
+  
+  
+}
+
+
+done.addEventListener('click',handler)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
