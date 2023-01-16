411c5c48951822c985ce0b037f96a2ad46e48389
kindtiger <seonbeom2@gmail.com>
2023-01-16 09:36:59 +0900

    [추가] copy 유틸 함수 만들기

--- client/main.js
+++ client/main.js
@@ -1,72 +1,59 @@
+import {
+  clearContents,
+  getInputValue,
+  getNode,
+  getRandom,
+  insertLast,
+  isNumericString,
+  addClass,
+  removeClass,
+  showAlert,
+  copy,
+} from './lib/index.js';
 
-import {clearContents, getInputValue, getNode, getRandom, insertLast, isNumericString,addClass,removeClass } from './lib/index.js'
-
-import { jujeobData } from "./data/data.js";
-
+import { jujeobData } from './data/data.js';
 
 const submit = getNode('#submit');
 const resultArea = getNode('.result');
 
-
-
-
-
-function clickSubmitHandler(e){
-
+function clickSubmitHandler(e) {
   e.preventDefault();
 
   let name = getInputValue('#nameField');
-  let list = jujeobData(name)
-  let pick = list[getRandom(list.length-1)];
+  let list = jujeobData(name);
+  let pick = list[getRandom(list.length - 1)];
 
-
-  
-  if(!name){
+  if (!name) {
     console.log('이름을 입력해 주세요!');
-    showAlert('.alert-error','잘못된 정보입니다.!',2000);
-    return 
+    showAlert('.alert-error', '잘못된 정보입니다.!', 2000);
+    return;
   }
 
-  if(isNumericString(name)){
+  if (isNumericString(name)) {
     console.log('제대로된 이름을 입력해주세요.');
-    return
+    showAlert('.alert-error', '정확한 이름을 입력해주세요!', 2000);
+    return;
   }
 
-  
-  clearContents(resultArea)
-  insertLast(resultArea,pick)
-  
+  clearContents(resultArea);
+  insertLast(resultArea, pick);
 }
 
+function clickCopyHandler(){
+  let text = resultArea.textContent;
+  // navigator.clipboard.writeText(text);
+  copy(text).then(()=>{
+    showAlert('.alert-success','클립보드 복사가 완료됐습니다.',2000)
+  })
+  
+  // 약속구문 
+  
+  // 약속 
+  // 다음 해야 할 일 
+}
 
-
-submit.addEventListener('click',clickSubmitHandler)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+submit.addEventListener('click', clickSubmitHandler);
+resultArea.addEventListener('click', clickCopyHandler);
 
 
 
