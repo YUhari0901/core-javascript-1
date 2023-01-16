b2e13634f9f93358d0172cf4acfcba153cb0f715
kindtiger <seonbeom2@gmail.com>
2023-01-16 09:51:55 +0900

    [추가] gsap 라이브러리 추가

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+/* global gsap */
+
 import {
   clearContents,
   getInputValue,
@@ -7,6 +9,7 @@ import {
   isNumericString,
   addClass,
   removeClass,
+  toggleClass,
   showAlert,
   copy,
 } from './lib/index.js';
@@ -26,11 +29,21 @@ function clickSubmitHandler(e) {
   if (!name) {
     console.log('이름을 입력해 주세요!');
     showAlert('.alert-error', '잘못된 정보입니다.!', 2000);
+
+
+    // GSAP 
+    gsap.fromTo(resultArea, 0.01, {x:-5}, {x:5, clearProps:"x", repeat:20})
+    // addClass(resultArea,'shake');
+    // setTimeout(() => {
+    //   removeClass(resultArea,'shake');
+    // }, 1000);
+
     return;
   }
 
   if (isNumericString(name)) {
     console.log('제대로된 이름을 입력해주세요.');
+    gsap.fromTo(resultArea, 0.01, {x:-5}, {x:5, clearProps:"x", repeat:20})
     showAlert('.alert-error', '정확한 이름을 입력해주세요!', 2000);
     return;
   }
